d7ce74c105102f39b80dafad68d963ac17c36b5b
Iara Baya <iarabayav@gmail.com>
2021-05-24 00:38:57 -0300

    add a table to show the data, styled with bootstrap

--- js unidad 2/main.js
+++ js unidad 2/main.js
@@ -1,27 +1,36 @@
-var nombreAlumno = "";
-var nota1;
-var nota2;
-var nota3;
-var promedio = 0;
+let nombreAlumno = "";
+let nota1;
+let nota2;
+let nota3;
+let promedio = 0;
 
 nombreAlumno = prompt("Ingrese nombre del alumno: ");
-
-if(typeof(nombreAlumno) === "string"){
-    console.log(nombreAlumno + ' es un string');
-}else{
-    console.log(nombreAlumno + "no es un string");
-}
 
 nota1 = prompt ("ingrese su primera nota: ");
 nota2 = prompt ("ingrese su segunda nota: ");
 nota3 = prompt ("ingrese su tercera nota: ");
-
 promedio = (parseInt(nota1) + parseInt(nota2) + parseInt(nota3))/3;
-
-console.log(promedio);
 
 alert("Su nombre es "+ nombreAlumno + " y su promedio es " + promedio);
 
-document.write("<h3>Su nombre es "+ nombreAlumno +"</h3>");
-document.write("<h2>Y su promedio es " + promedio + "</h2>");
+document.getElementById("info").innerHTML = `<table class="table">
+<thead class="table-dark">
+  <tr>
+    <th scope="col">Nombre</th>
+    <th scope="col">Nota 1</th>
+    <th scope="col">Nota 2</th>
+    <th scope="col">Nota 3</th>
+    <th scope="col">Promedio</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <th scope="row">${nombreAlumno}</th>
+    <td>${nota1}</td>
+    <td>${nota2}</td>
+    <td>${nota3}</td>
+    <td>${promedio}</td>
+  </tr>
+  </tbody>
+  </table>`;
 
